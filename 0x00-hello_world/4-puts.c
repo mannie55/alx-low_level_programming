a37d42623bdf8989be66d65a8b078dd792676e70
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-13 08:34:39 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a program using put function
+ * Return: 0(Program ran successfully!)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
