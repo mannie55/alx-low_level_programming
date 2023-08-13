511e7a3e185349d55dbd79bcb3708e1b9802391c
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-13 08:55:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a C program using a printf function
+ * Return: 0(Program ran successfully!)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
