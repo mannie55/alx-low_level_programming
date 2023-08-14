1acf25704c038b4c97e49288c993cfae95dabe57
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-14 01:01:39 -0700

    Failure is the condiment that gives success a flavour

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all the single digit number of base10
+ * Return: 0(Program ran successfully!)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	printf("%d", num);
+		printf("\n");
+	return (0);
+}
