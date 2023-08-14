79be042470b489a26301b725d2b1a7228eda90d0
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-13 23:51:33 -0700

    life is beautiful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - C program that prints a line with standard error
+ * Return: 1(Program ran successfully)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
