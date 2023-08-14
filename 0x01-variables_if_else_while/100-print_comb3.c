472204c4eea1f392684a2898b6f59d14ce8992c2
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-14 07:30:08 -0700

    Dream big and dear to fail

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all possible different combinations
+ * Return: 0(Program ran successfully!)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == '8' && b == '9')
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
