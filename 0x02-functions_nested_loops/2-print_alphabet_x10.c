855c34fd5396bc364e08baa606323bb59747568b
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-16 14:56:59 -0700

    prints alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - this function prints alphabets
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	_putchar(alph);
+	if (alph == 'z')
+	_putchar('\n');
+	}
+	}
+}
