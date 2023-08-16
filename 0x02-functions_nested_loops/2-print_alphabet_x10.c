66e41d19c7c083c0aa71f014cafa0f14f356184c
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-16 14:59:46 -0700

    fixed betty issues

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - this function prints alphabets
+ * print_alphabet_x10 - this function prints alphabets
  * Return: 0 always
  */
 void print_alphabet_x10(void)
 {
 	char alph;
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 	for (alph = 'a'; alph <= 'z'; alph++)
