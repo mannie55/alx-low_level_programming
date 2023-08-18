5001686e9c1ab238d1146265b9b44e089fcfeb06
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-18 16:46:32 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a tringle
+* @size: value to check
+* prints newline if size is less than zero
+* Return: zero
+*/
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
