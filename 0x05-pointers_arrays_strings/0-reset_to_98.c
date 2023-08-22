c81765e45338a3df083f70f262cb9c9125f3ebe0
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-22 10:45:36 -0700

    point of correction

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this funtion updates the pointer integer
+ * @n: pointer to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+<h1>POINTERS AND ARRAYS</h1>
+This directory contains project on pointers
+arrays
+strings.
