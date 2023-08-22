8c7726f19dcefc86f62134df2d5c373c3a68d82f
Ogbonna Nnamdi <mannnie55@gmail.com>
2023-08-22 13:19:10 -0700

    strlrn

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function to count string
+ * @s: value
+ * Return: this returns i
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
